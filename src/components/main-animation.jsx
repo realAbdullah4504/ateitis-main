2326120dc40f4e4b7ae93e2477c1644209705f74
realAbdullah4504 <abdullahjaved4504@gmail.com>
2025-03-13 10:51:51 +0000

    animation fix

--- src/components/main-animation.jsx
+++ src/components/main-animation.jsx
@@ -1,24 +1,6 @@
 import React from "react";
-import Animated from "../images/animation.svg";
+import { ReactComponent as AnimatedSVG } from "../images/animation.svg";
 
-import styled from "styled-components";
-
-const StyledMainAnimation = styled.div`
-  /* width: 80%; */
-  /* height: 80%; */
-  /* display: flex; */
-  /* align-items: end; */
-  /* margin: auto; */
-  /* margin-top: 11em; */
-`;
-
-function MainAnimation() {
-  return (
-    // <StyledMainAnimation>
-    <object type="image/svg+xml" data={Animated} className="svg" alt="Intro main page welcome">
-      svg-animation
-    </object>
-    // </StyledMainAnimation>
-  );
+export default function MainAnimation() {
+  return <AnimatedSVG className="svg" />;
 }
-export default MainAnimation;
