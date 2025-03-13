1ab6ac9711a812315ac95d6c117bb43c1bbfaddc
realAbdullah4504 <abdullahjaved4504@gmail.com>
2025-03-13 19:10:49 +0000

    client testimonial done

--- src/components/testimonio.jsx
+++ src/components/testimonio.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { Col } from "react-bootstrap";
-import { GatsbyImage,getImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import line from "../images/line2.png";
 import linkedin from "../images/inLogo.png";
 
@@ -18,14 +18,14 @@ export default function Testimonio({ cliente, carousel }) {
         <div className="burbuja">
           <div className="texto">{texto}</div>
         </div>
-        <img  src={line} style={{
-          width:"100%"
+        <img src={line} style={{
+          width: "100%",
         }}></img>
         <div className="foto-links">
           <div className="datos">
             <div className="nombre">{nombre}</div>
             <div className="puesto">{puestoCliente}</div>
-          <img src={linkedin} className="linkedin" width={32}></img>
+            <img src={linkedin} className="linkedin" width={32}></img>
           </div>
           <GatsbyImage image={getImage(imagen)} imgClassName="avatar"></GatsbyImage>
         </div>
